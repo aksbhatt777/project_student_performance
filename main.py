b8035ffb6bb1c07c7847bfe248b14ad12183cfab
Ubuntu <ubuntu@ip-172-31-34-195.ap-southeast-1.compute.internal>
2026-08-16 16:48:14 +0000

    Data Trasformation and Model Trainer added

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_ingestion import DataIngestionConfig,DataIngestion
+from src.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    model_trainer=ModelTrainer()
+    print(model_trainer.Initiate_model_trainer(train_arr,test_arr))
